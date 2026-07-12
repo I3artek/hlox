f525f6b92df0defd79beeb9eb32948e59849190d
Bartlomiej Wzorek <i3artek3@gmail.com>
2026-07-12 15:06:06 +0200

    fix erros in evaluation
    Signed-off-by: Bartlomiej Wzorek <i3artek3@gmail.com>

--- src/Evals.hs
+++ src/Evals.hs
@@ -1,7 +1,9 @@
 module Evals where
 
 import Control.Monad.Error.Class (throwError)
+import Control.Monad.Except (ExceptT, runExceptT)
 import Control.Monad.State
+import Data.Data (typeOf)
 import Exprs (BinaryExpr (..), Expr (..), GroupingExpr (..), LiteralExpr (..), UnaryExpr (..))
 import Stmts (Stmt (..))
 import Tokens (Token (..))
@@ -21,88 +23,85 @@ instance Show Value where
   show (LoxBool b) = show b
   show LoxNil = ""
 
-type MaybeValue = Either String Value
+type RuntimeError = String
 
 -- placeholder type
 type ScopeState = Int
 
-type Scope a = StateT ScopeState IO a
+type Scope a = ExceptT RuntimeError (StateT ScopeState IO) a
 
-runtimeError :: String -> Scope Bool
+runtimeError :: String -> Scope ()
 runtimeError msg = do
-  lift $ putStrLn ("Runtime Error: " ++ msg)
-  return False
+  throwError $ "Runtime Error: " ++ msg
 
-runtimeSuccess :: Scope Bool
-runtimeSuccess = return True
+evalError :: String -> Scope Value
+evalError msg = do
+  throwError $ "Runtime Error: " ++ msg
 
-execScope :: [Stmt] -> Scope Bool
-execScope [] = return True
+execScope :: [Stmt] -> Scope ()
+execScope [] = return ()
 execScope (s : rest) = do
-  status <- execStmt s
-  case status of
-    True -> execScope rest
-    False -> return False
+  execStmt s
+  execScope rest
 
-execProgram :: [Stmt] -> IO Bool
-execProgram stmts = evalStateT (execScope stmts) (0)
+execProgram :: [Stmt] -> IO ()
+execProgram stmts = do
+  errors <- evalStateT (runExceptT $ execScope stmts) (0)
+  case errors of
+    Left err -> print err
+    Right _ -> return ()
 
-execStmt :: Stmt -> Scope Bool
+execStmt :: Stmt -> Scope ()
 execStmt (ExprStmt e) = do
-  let val = evalExpr e
-  case val of
-    Left err -> runtimeError err
-    Right _ -> runtimeSuccess
+  _ <- evalExpr e
+  return ()
 execStmt (PrintStmt e) = do
-  let val = evalExpr e
-  case val of
-    Left err -> runtimeError err
-    Right v -> do
-      lift $ print v
-      runtimeSuccess
+  val <- evalExpr e
+  lift $ lift $ print val
+  return ()
 
-evalExpr :: Expr -> MaybeValue
+evalExpr :: Expr -> Scope Value
 evalExpr (Binary e) = do
   left <- evalExpr $ binaryLeft e
   right <- evalExpr $ binaryRight e
   let op = binaryOperator e
   evalBinaryExpr left op right
 evalExpr (Grouping e) = evalExpr $ groupedExpression e
-evalExpr (Literal e) = Right $ evalLiteral $ value e
+evalExpr (Literal e) = evalLiteral $ value e
 evalExpr (Unary e) = do
   right <- evalExpr $ unaryRight e
   let op = unaryOperator e
-  Right $ evalUnaryExpr op right
+  evalUnaryExpr op right
 
-evalBinaryExpr :: Value -> Token -> Value -> MaybeValue
-evalBinaryExpr (LoxString a) PLUS (LoxString b) = Right $ LoxString (a ++ b)
-evalBinaryExpr (LoxNumber a) PLUS (LoxNumber b) = Right $ LoxNumber (a + b)
-evalBinaryExpr (LoxString a) PLUS v = Right $ LoxString (a ++ show v)
-evalBinaryExpr (v) PLUS (LoxString b) = Right $ LoxString (show v ++ b)
-evalBinaryExpr (LoxNumber a) MINUS (LoxNumber b) = Right $ LoxNumber (a - b)
-evalBinaryExpr (LoxNumber _) SLASH (LoxNumber 0) = throwError "Division by zero not allowed!"
-evalBinaryExpr (LoxNumber a) SLASH (LoxNumber b) = Right $ LoxNumber (a / b)
-evalBinaryExpr (LoxNumber a) STAR (LoxNumber b) = Right $ LoxNumber (a * b)
-evalBinaryExpr v1 BANG_EQUAL v2 = Right $ LoxBool $ v1 /= v2
-evalBinaryExpr v1 EQUAL_EQUAL v2 = Right $ LoxBool $ v1 == v2
-evalBinaryExpr v1 GREATER v2 = Right $ LoxBool $ v1 > v2
-evalBinaryExpr v1 GREATER_EQUAL v2 = Right $ LoxBool $ v1 >= v2
-evalBinaryExpr v1 LESS v2 = Right $ LoxBool $ v1 < v2
-evalBinaryExpr v1 LESS_EQUAL v2 = Right $ LoxBool $ v1 <= v2
-evalBinaryExpr t1 op t2 = throwError $ "Not possible to perform " ++ show t1 ++ " " ++ show op ++ " " ++ show t2
+evalBinaryExpr :: Value -> Token -> Value -> Scope Value
+evalBinaryExpr (LoxString a) PLUS (LoxString b) = return $ LoxString (a ++ b)
+evalBinaryExpr (LoxNumber a) PLUS (LoxNumber b) = return $ LoxNumber (a + b)
+evalBinaryExpr (LoxString a) PLUS v = return $ LoxString (a ++ show v)
+evalBinaryExpr (v) PLUS (LoxString b) = return $ LoxString (show v ++ b)
+evalBinaryExpr (LoxNumber a) MINUS (LoxNumber b) = return $ LoxNumber (a - b)
+evalBinaryExpr (LoxNumber _) SLASH (LoxNumber 0) = evalError "Division by zero not allowed!"
+evalBinaryExpr (LoxNumber a) SLASH (LoxNumber b) = return $ LoxNumber (a / b)
+evalBinaryExpr (LoxNumber a) STAR (LoxNumber b) = return $ LoxNumber (a * b)
+evalBinaryExpr v1 BANG_EQUAL v2 = return $ LoxBool $ v1 /= v2
+evalBinaryExpr v1 EQUAL_EQUAL v2 = return $ LoxBool $ v1 == v2
+evalBinaryExpr v1 GREATER v2 = return $ LoxBool $ v1 > v2
+evalBinaryExpr v1 GREATER_EQUAL v2 = return $ LoxBool $ v1 >= v2
+evalBinaryExpr v1 LESS v2 = return $ LoxBool $ v1 < v2
+evalBinaryExpr v1 LESS_EQUAL v2 = return $ LoxBool $ v1 <= v2
+evalBinaryExpr t1 op t2 = evalError $ "Not possible to perform " ++ show t1 ++ " " ++ show op ++ " " ++ show t2
 
-evalLiteral :: Token -> Value
-evalLiteral (STRING s) = LoxString s
-evalLiteral (NUMBER n) = LoxNumber n
-evalLiteral (IDENTIFIER x) = LoxObject x
-evalLiteral FALSE = LoxBool False
-evalLiteral TRUE = LoxBool True
-evalLiteral _ = LoxNil
+evalLiteral :: Token -> Scope Value
+evalLiteral (STRING s) = return $ LoxString s
+evalLiteral (NUMBER n) = return $ LoxNumber n
+evalLiteral (IDENTIFIER x) = return $ LoxObject x
+evalLiteral FALSE = return $ LoxBool False
+evalLiteral TRUE = return $ LoxBool True
+evalLiteral _ = return LoxNil
 
-evalUnaryExpr :: Token -> Value -> Value
-evalUnaryExpr MINUS (LoxNumber b) = LoxNumber (-b)
-evalUnaryExpr BANG t = LoxBool $ not $ isTruthy t
-evalUnaryExpr op t = error $ "Not possible to perform " ++ show op ++ " " ++ show t
+evalUnaryExpr :: Token -> Value -> Scope Value
+evalUnaryExpr MINUS (LoxNumber b) = return $ LoxNumber (-b)
+evalUnaryExpr BANG t = return $ LoxBool $ not $ isTruthy t
+evalUnaryExpr op t = evalError $ "Not possible to perform " ++ show op ++ " " ++ show t
 
 isTruthy :: Value -> Bool
 isTruthy (LoxBool False) = False
